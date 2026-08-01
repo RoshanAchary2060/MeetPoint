dfea2cde7a804fc3266c4641f0fbd151dfe932d4
Roshan Acharya <roshanacharya2060@gmail.com>
2026-08-01 12:09:07 +0545

    l

--- frontend/src/components/UserProfileInfo.jsx
+++ frontend/src/components/UserProfileInfo.jsx
@@ -6,10 +6,13 @@ const UserProfileInfo = ({ user, posts, profileId, setShowEdit }) => {
     return (
         <div className='relative py-4 px-6 md:px-8 bg-white'>
             <div className='flex flex-col md:flex-row items-start gap-6'>
-                <div className='w-32 h-32 border-4 border-white shadow-lg absolute
-                -top-16 rounded-full'>
-                    <img src={user.profile_picture} className='absolute rounded-full z-2' alt="" />
-                </div>
+              <div className="absolute -top-16 left-8 w-32 h-32 rounded-full overflow-hidden border-4 border-white shadow-lg bg-white">
+                <img
+                  src={user.profile_picture}
+                  alt={user.full_name}
+                  className="w-full h-full object-cover"
+                />
+              </div>
                 <div className='w-full pt-15 md:pt-0 md:pl-36'>
                     <div className='flex flex-col md:flex-row items-start justify-between'>
                         <div>
