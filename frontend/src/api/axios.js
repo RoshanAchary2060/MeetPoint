63b952b19b24d74077ba7b00cea1b8a76aaee398
Roshan Acharya <roshanacharya2060@gmail.com>
2026-08-01 22:22:46 +0545

    ok

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://meetpoint-server.vercel.app/api"
+  baseURL: "https://meetpoint-server.vercel.app"
 });
 
 export default api;
