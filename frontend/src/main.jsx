01367b89a4a1f39bf5909d05f26abbd6111b1ec1
Roshan Acharya <roshanacharya2060@gmail.com>
2026-08-18 19:45:08 +0545

    ok

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "./app/store.js";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { CallProvider } from "./context/callContext.jsx";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
